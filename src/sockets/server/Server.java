b7f8816705645a9ff8a217768ffbd14ad3b60dac
Malwine <malwine@mailbox.org>
2016-10-19 13:02:17 +0200

    Set up server class

--- src/sockets/server/Server.java
+++ src/sockets/server/Server.java
@@ -1,14 +1,33 @@
 package sockets.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Server {
-	ServerSocket serverSocket = new ServerSocket(8080);
-    while(true) {
-		Socket clientSocket = serverSocket.accept();
-		InputStream input = clientSocket.getInputStream();
-		byte[] data = new byte[1024];
-		int numBytes = 0;
-		numBytes = input.read(data);
-		clientSocket.close();
+	
+	public static void server() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(8080);
+    	
+        while(true) {
+    		Socket clientSocket = serverSocket.accept();
+    		InputStream input = clientSocket.getInputStream();
+    		byte[] data = new byte[1024];
+    		int numBytes = 0;
+    		numBytes = input.read(data);
+    		clientSocket.close();
+    	}
 	}
+	
+    public static void main(String[] args) {
+    	
+    	try {
+			server();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
 }
 
