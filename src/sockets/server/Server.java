a66676015837aff6287904c346c45fc09af27e61
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-10-19 15:31:43 +0200

    remove unused dependencies

--- src/sockets/server/Server.java
+++ src/sockets/server/Server.java
@@ -2,11 +2,8 @@ package sockets.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
