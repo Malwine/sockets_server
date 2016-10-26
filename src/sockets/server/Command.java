9b3cc65d9e1f9e2a2fe4e3433c4efde7f05829d6
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-10-26 17:14:55 +0200

    remove todo comments

--- src/sockets/server/Command.java
+++ src/sockets/server/Command.java
@@ -4,7 +4,6 @@ public interface Command {
 	
 	// Override this method in all command classes that implement the interface 
 	// by selecting in the menu "Source" -> "Override/Implement Methods"
-	// TODO Create classes that implement "Command": Multiply, Subtract and Hello
 	
 	public default String process(String[] args) {
 		return null;
