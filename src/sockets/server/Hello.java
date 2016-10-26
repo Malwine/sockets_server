02985752540e74373e3d0d658eb0cac2075220d1
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-10-26 17:13:45 +0200

    implement basic hello functionality

--- src/sockets/server/Hello.java
+++ src/sockets/server/Hello.java
@@ -4,8 +4,20 @@ public class Hello implements Command {
 
 	@Override
 	public String process(String[] args) {
-		// TODO Auto-generated method stub
-		return Command.super.process(args);
+		if (args.length == 0) {
+			return "Hello World!";
+		}
+		else {
+			return "Hello " + args[0] + "!";
+		}
 	}
+	
+	//simple tests
+/*	public static void main(String[] args) {
+		Hello h = new Hello();
+		System.out.println(h.process(new String[]{}));
+		System.out.println(h.process(new String[]{"3", "2"}));
+		System.out.println(h.process(new String[]{"10", "6", "5"}));
+	}*/
 
 }
