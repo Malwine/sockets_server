c7f24c36dc64acef8eea01d5928ec940d9cf49f6
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-10-26 17:14:08 +0200

    implement subtraction functionality and tests

--- src/sockets/server/Subtract.java
+++ src/sockets/server/Subtract.java
@@ -4,8 +4,27 @@ public class Subtract implements Command {
 
 	@Override
 	public String process(String[] args) {
-		// TODO Auto-generated method stub
-		return Command.super.process(args);
+		// for now we test the length of args, but we would like to have a fallback if the array is empty in class Protocol
+		if (args.length == 0) {
+			return "Error: No parameters given";
+		}
+		else {
+			Double result = Double.parseDouble(args[0]);
+		
+		for (int i=1; i<args.length; i++) {
+			result -= Double.parseDouble(args[1]);
+			}
+		return result.toString();
+		}
+		
 	}
-
+	
+	//simple tests
+/*	public static void main(String[] args) {
+		Subtract s = new Subtract();
+		System.out.println(s.process(new String[]{}));
+		System.out.println(s.process(new String[]{"3", "2"}));
+		System.out.println(s.process(new String[]{"10", "6", "5"}));
+	}
+*/
 }
